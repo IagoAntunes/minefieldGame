c4d0361e14bf976b0e443396a8b51efb24975993
IagoAntunes <iagoantunes.f@gmail.com>
2022-12-27 09:33:48 -0300

    unity test board

---
+++ test/models/tabuleiro_test.dart
@@ -0,0 +1,25 @@
+import 'package:campominado/models/tabuleiro.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test(
+    'ganharJogo',
+    () {
+      Tabuleiro tabuleiro = Tabuleiro(
+        linhas: 2,
+        colunas: 2,
+        qtdeBombas: 0,
+      );
+      tabuleiro.campos[0].minar();
+      tabuleiro.campos[3].minar();
+
+      tabuleiro.campos[0].alternarMarcacao();
+      tabuleiro.campos[1].abrir();
+
+      tabuleiro.campos[3].alternarMarcacao();
+      tabuleiro.campos[2].abrir();
+
+      expect(tabuleiro.resolvido, isTrue);
+    },
+  );
+}
